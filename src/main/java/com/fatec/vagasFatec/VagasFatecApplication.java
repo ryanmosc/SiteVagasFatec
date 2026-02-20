046fa1d72fa10b74ce339a8fc775c41df848ab74
ryanmosc <ryanoliveiramosc.com.098@gmail.com>
2026-02-20 19:36:31 -0300

    Primeiro commit

---
+++ src/main/java/com/fatec/vagasFatec/VagasFatecApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.vagasFatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagasFatecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagasFatecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/vagasFatec/VagasFatecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.vagasFatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagasFatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
