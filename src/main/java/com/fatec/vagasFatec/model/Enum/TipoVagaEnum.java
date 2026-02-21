08443fe29ee4ab40ca0c346fedab9b7bd253ffc9
ryanmosc <ryanoliveiramosc.com.098@gmail.com>
2026-02-21 11:03:50 -0300

    VagaModel

---
+++ src/main/java/com/fatec/vagasFatec/model/Enum/TipoVagaEnum.java
@@ -0,0 +1,8 @@
+package com.fatec.vagasFatec.model.Enum;
+
+public enum TipoVagaEnum {
+    ESTAGIO,
+    CLT,
+    PJ,
+    FREELANCE
+}
--- src/main/java/com/fatec/vagasFatec/model/Vaga.java
+++ src/main/java/com/fatec/vagasFatec/model/Vaga.java
@@ -1,4 +1,43 @@
 package com.fatec.vagasFatec.model;
 
+import com.fatec.vagasFatec.model.Enum.TipoVagaEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "vaga")
 public class Vaga {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(name = "tituloVaga")
+    private String tituloVaga;
+
+    @Column(name = "descricaoVaga")
+    private String descricaoVaga;
+
+    @Column(name = "cursoVaga")
+    private String cursovaga;
+
+    @Column(name = "tipoVaga")
+    @Enumerated(EnumType.STRING)
+    private TipoVagaEnum tipoVagaEnum;
+
+    @Column(name = "dataPublicacaoVaga")
+    private LocalDateTime dataPublicacaoVaga;
+
+    @Column(name = "statusVaga")
+    private Boolean statusvaga = Boolean.TRUE;
+
+
 }
