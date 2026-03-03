8af7cb48b969e7ba900504be038079ec5a1df4e9
ryanmosc <ryanoliveiramosc.com.098@gmail.com>
2026-03-03 08:46:52 -0300

    Adicionei o service de enviar e-mail

---
+++ src/main/java/com/fatec/vagasFatec/utils/EmailSender.java
@@ -0,0 +1,30 @@
+package com.fatec.vagasFatec.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class EmailSender {
+
+    private final JavaMailSender mailSender;
+    private final String remetente = "contato@voluntariosdasaude.com.br";
+
+
+    public void enviarEmail(String destinatario, String assunto, String corpo){
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setFrom(remetente);
+        mailMessage.setTo(destinatario);
+        mailMessage.setSubject(assunto);
+        mailMessage.setText(corpo);
+
+        mailSender.send(mailMessage);
+    }
+}
